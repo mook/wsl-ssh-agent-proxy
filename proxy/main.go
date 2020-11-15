3f6a6b22ac854d84387d22e6c651d90ac0e3df57
Mook <mook@users.noreply.github.com>
2020-11-15 09:09:13 -0800

    Refactor logging in the Windows side

--- proxy/main.go
+++ proxy/main.go
@@ -22,54 +22,52 @@ type CloseWriter interface {
 	CloseWrite() error
 }
 
+func log(format string, args ...interface{}) {
+	if *verbose {
+		fmt.Fprintf(os.Stderr, format+"\n", args...)
+	}
+}
+
 func main() {
 	flag.Parse()
 	pipe, err := winio.DialPipe(*pipeName, nil)
 	if err != nil {
-		if *verbose {
-			fmt.Fprintf(os.Stderr, "Could not open ssh-agent pipe: %s\n", err)
-		}
+		log("Could not open ssh-agent pipe: %s", err)
 		os.Exit(1)
 	}
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		_, err := io.Copy(os.Stdout, pipe)
-		if err != nil && *verbose {
-			fmt.Fprintf(os.Stderr, "Error reading from ssh agent: %s\n", err)
+		if err != nil {
+			log("Error reading from ssh agent: %s", err)
 		}
 		wg.Done()
 	}()
 	go func() {
 		_, err := io.Copy(pipe, os.Stdin)
-		if err != nil && *verbose {
-			fmt.Fprintf(os.Stderr, "Could not write to ssh agent: %s\n", err)
+		if err != nil {
+			log("Could not write to ssh agent: %s", err)
 		}
 		if messagePipe, ok := pipe.(CloseWriter); ok {
 			err = messagePipe.CloseWrite()
-			if err != nil && *verbose {
-				fmt.Fprintf(os.Stderr, "Could not close pipe to ssh agent: %s\n", err)
+			if err != nil {
+				log("Could not close pipe to ssh agent: %s", err)
 			}
 		} else {
-			if *verbose {
-				fmt.Fprintf(os.Stderr, "Warning: pipe is not in message mode\n")
-			}
+			log("Warning: pipe is not in message mode")
 			_, err = pipe.Write(nil)
-			if err != nil && *verbose {
-				fmt.Fprintf(os.Stderr, "Could not tidy up pipe to ssh agent: %s\n", err)
+			if err != nil {
+				log("Could not tidy up pipe to ssh agent: %s", err)
 			}
 		}
 		err = pipe.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not close SSH agent pipe: %s\n", err)
+			log("Could not close SSH agent pipe: %s", err)
 		}
 		wg.Done()
 	}()
-	if *verbose {
-		fmt.Fprintf(os.Stderr, "SSH agent pipe established.\n")
-	}
+	log("SSH agent pipe established.")
 	wg.Wait()
-	if *verbose {
-		fmt.Fprintf(os.Stderr, "SSH agent proxy terminated.\n")
-	}
+	log("SSH agent proxy terminated.")
 }
